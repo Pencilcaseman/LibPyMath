e07358075b65525f61183593ae7473fb5bdff781
Toby Davis <pencilcaseman@gmail.com>
2020-09-25 21:49:27 +0100

    Bug fixes

--- setup.py
+++ setup.py
@@ -203,7 +203,7 @@ ext_modules = [
 
 setup(
     name="libpymath",
-    version="0.3.4",
+    version="0.3.5",
     description="A general purpose Python math module",
     long_description=long_description,
     long_description_content_type='text/markdown',
